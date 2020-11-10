5483796ba5878bc6f44c5665a5f9a6336a452c1b
Florian Tetaud <florian.tetaud@gmail.com>
2020-11-10 17:03:58 +0100

    add game instance and ping

--- Scenes/Server.gd
+++ Scenes/Server.gd
@@ -6,8 +6,15 @@ extends Node
 # var b = "text"
 var peer =  NetworkedMultiplayerENet.new()
 var ip = "pixpong.ddns.net"
+#var ip = "5.48.149.215"
+#var ip = "127.0.0.1"
 var port = 1025;
 
+var ping = false
+var pingrequest = false
+var connected = false
+var ping_value : float =  999;
+var ping_value_tmp : float = 0.00000
 #datas from server :
 var ball_info_json  = \
 	"{"\
@@ -27,19 +34,40 @@ func _ready():
 func _process(delta):
 	var pongnode = get_parent()
 	_ReturnPlayersControl(pongnode.KEY_UP_pressed,pongnode.KEY_DOWN_pressed)
-	
+	ping_server(delta)
 
 func _OnConnectionFailed():
 	print("connection failed")
-	
+	connected = false
 func _OnConnect():
 	print("sucess to connect")
+	connected = true
 	
 func _server_input_event(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_SPACE:
-			rpc_id(1,"_StartGame")
+			if(!get_parent().playing):
+				rpc_id(1,"_StartGame")
+
+func ping_server(delta):
+	if(!ping and connected and !pingrequest):
+		pingrequest = true
+		ping_value_tmp=0
+		rpc_unreliable_id(1,"ping")
+	else:
+		if(ping):
+			pingrequest =false
+			ping =false
+			ping_value = ping_value_tmp
+			$Ping.text = "ping = "+str(ping_value);
+		else:
+			ping_value_tmp+=delta
+
+
+remote func _return_ping():
+	ping = true
 	
+
 remote func _return_server_ball_info(dx,dy,playing):
 	ball_info_json = \
 	"{"\
@@ -54,7 +82,13 @@ remote func _return_players_position(p1x,p1y,p2x,p2y):
 func _ReturnPlayersControl(KEY_UP_pressed,KEY_DOWN_pressed):
 	rpc_unreliable_id(1,"_ReturnPlayersControl",str(KEY_UP_pressed),str(KEY_DOWN_pressed))
 	pass
-	
+
+remote func _return_score_info(p1score,p2score):
+	get_parent().update_score(p1score,p2score);
+
+remote func _return_DisplayMessage(message,visible):
+	get_parent().display_message(message,visible)
+
 func get_ball_position():
 	return ball_info_json
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -17,10 +17,9 @@ var playing = false
 var score_event = false
 var game_done = false
 
-const SPACE_TO_PLAY = "Press SPACE to Play!"
 const P1_WIN = "Player 1 won!"
 const P2_WIN = "Player 2 won!"
-var message = SPACE_TO_PLAY
+
 var KEY_UP_pressed = false;
 var KEY_DOWN_pressed = false;
 # Called when the node enters the scene tree for the first time.
@@ -28,11 +27,8 @@ var KEY_DOWN_pressed = false;
 
 func _ready():
 	set_ball()
-#	$Player1.set_paddle_position(p1_x, p1_y)
-#	$Player2.set_paddle_position(p2_x, p2_y)
-	display_message()
-	update_score()
-
+	display_message("PRESS SPACEBAR","True")
+#	update_score("0","0")
 
 
 
@@ -46,9 +42,7 @@ func _process(delta):
 	handle_movement_input()
 	$Player1.set_paddle_position(p1_x, p1_y)
 	$Player2.set_paddle_position(p2_x, p2_y)
-	#check_point_scored()
-	#handle_score_event()
-	handle_game_end()
+
 
 func _setVariable(var1x,var2x,var1y,var2y):
 	p1_x=var1x
@@ -57,28 +51,13 @@ func _setVariable(var1x,var2x,var1y,var2y):
 	p2_y=var2y
 	pass
 
-#func play():
-#	if game_done: # if game was done, reset states to start a fresh game
-#		game_done = false
-#		p1_score = 0
-#		p2_score = 0
-#		message = SPACE_TO_PLAY
-#		update_score()
-#	playing = true
-#	ball.set_playing(playing)
-#	$DisplayMessage.visible = false
+func remove_ball():
+	remove_child(ball)
 
 
-#func check_point_scored():
-#	if ball.position.x <= 0:
-#		score_event = true
-#		p2_score += 1
-#	if ball.position.x >= 1000:
-#		score_event = true
-#		p1_score += 1
-#	update_score()
-#	if p1_score == 5 or p2_score == 5:
-#		game_done = true
+func set_ball():
+	ball = BALL_NODE.instance()
+	add_child(ball)
 
 
 func handle_movement_input():
@@ -94,48 +73,11 @@ func handle_movement_input():
 	KEY_DOWN_pressed = false;
 	return false
 
-		#p1_y += 300 * delta
-
-
-#func handle_score_event():
-#	if score_event:
-#		remove_ball()
-#		set_ball()
-#		reset_paddle_positions()
-#		display_message()
-#		playing = false
-#		score_event = false
-#		$ScoreSound.play()
-
-
-func remove_ball():
-	remove_child(ball)
-
-
-func set_ball():
-	ball = BALL_NODE.instance()
-	add_child(ball)
-
-
-func reset_paddle_positions():
-	p1_y = DEFAULT_Y
-	p2_y = DEFAULT_Y
-
-
-func update_score():
+func update_score(p1_score,p2_score):
 	$Player1Score.text = str(p1_score)
 	$Player2Score.text = str(p2_score)
 
-
-func handle_game_end():
-	if game_done:
-		if p1_score == 5:
-			message = P1_WIN
-		else:
-			message = P2_WIN
-		display_message()
-
-
-func display_message():
+func display_message(message,s_visible):
 	$DisplayMessage.text = message
-	$DisplayMessage.visible = true
+	var b_visible  = Helpers.convert_string_to_bool(s_visible)
+	$DisplayMessage.visible = b_visible
